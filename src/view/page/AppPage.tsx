ea26fc3b23b054fe2b2d4e5acfdadc4111e4c300
Julian Aryo <julian.aryo1989@gmail.com>
2023-02-12 10:41:01 +0800

    WIP

--- src/view/page/AppPage.tsx
+++ src/view/page/AppPage.tsx
@@ -3,8 +3,9 @@ import { Outlet } from "react-router";
 function AppPage() {
   return (
     <>
-      <div>App Page</div>
-      <Outlet />
+      <div className="w-full h-screen">
+        <div className="drop-shadow h-14 w-full bg-surface absolute top-0"></div>
+      </div>
     </>
   );
 }
